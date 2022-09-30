cd15090051c35a2d6cd863057da6aa316c20e9bb
elyesnasri <enasri@1drop.de>
2022-09-30 15:44:54 +0200

    added apollo client

--- QuizApp.Web/ClientApp/src/main.ts
+++ QuizApp.Web/ClientApp/src/main.ts
@@ -3,12 +3,20 @@ import App from './App.vue'
 import router from './router'
 import { loadFonts } from './plugins/webfontloader'
 import { createVuetify } from 'vuetify';
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
 
 loadFonts()
 
+const cache = new InMemoryCache()
+const apolloClient = new ApolloClient(
+  {
+    cache,
+    uri: '<https://localhost:7199/graphql>'
+  }
+)
+
 const vuetify = createVuetify()
 const app = createApp(App)
-
 app.use(router)
   .use(vuetify)
 
